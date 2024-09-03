d4b0e661e3e321cbab3b1d9197e5e9e7f07b137e
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2024-09-03 17:42:17 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,66 @@
+// Task 1
+
+// const colors = {
+//     red: "#ff0000",
+//     green: "#00ff00",
+//     blue: "#0000ff",
+//     "golden rod": '#daa520'
+// };
+// let newColor = colors["golden rod"];
+// console.log(newColor);
+
+// Task 2
+
+// const car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020
+// };
+// console.log(car);
+// console.log(car["passenger capacity"] = 5);
+// console.log(car);
+
+// Task 3
+
+// const student = {
+//     name: "Hamim Sakep",
+//     id: 5421,
+//     physics: {
+//         subject: "HSC Physics",
+//         author: "Shahjahan Tapan",
+//         marks: 30
+//     }
+// };
+// console.log(student.physics.marks);
+
+// Task 4
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+// **** rule-1 ****
+// let count = Object.keys(student).length;
+// console.log(count);
+
+// **** rule-2****
+// let count = 0;
+// for(keys in student){
+//   count++;
+//   console.log(count);
+// }
+
+// Task 5
+
+// let myObject = {
+//     name: 'John Doe',
+//     age: 25,
+//     city: 'Example City',
+//     isStudent: true
+// };
+// for(key in myObject){
+//    console.log( key , ':' , myObject[key] , "Type :" , typeof myObject[key]);
+// }
---
+++ string.js
@@ -0,0 +1,45 @@
+// Task 1
+
+// let name = 'ahbabuzzaman';
+// let count = 0;
+// for(i = 0; i < name.length; i++){
+//     if (name[i] === 'a') {
+//         count++;       
+//     }
+// }
+// console.log(count);
+
+// Task 2
+
+// let name = 'Ahbabuzzaman';
+// let count = 0;
+// for(i = 0; i < name.length; i++){
+//     if (name[i] === 'a') {
+//         count++;       
+//     } 
+// }
+//  console.log(count);
+
+// Task 3
+
+// let name = 'i am learning javascript'
+// for(char of name){
+//     if (char === 'a' || char === 'e' || char === 'i' === char === 'o' || char === 'u') {
+//         console.log(name , ':' , char );  
+//     }
+// }
+
+// let vowel = name.includes('a' || 'e' || 'i' || 'o' || 'u');
+// console.log(vowel);
+
+// Task 4
+
+// let name = 'X - man';
+// let replace = name[1] = 'x'
+
+// console.log(name);
+// console.log(replace);
+
+// let name = 'ahbab';
+// let newName = name.toUpperCase();
+// console.log(newName)
